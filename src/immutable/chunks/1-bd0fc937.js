ee47cc42851c8912eb6bb0ca60bff758e3192c0d
Mandakara Sadewa <mdswa@zxce3.net>
2023-10-07 20:44:26 +0000

    Update GitHub Pages

---
+++ src/immutable/chunks/1-bd0fc937.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_error.svelte-36a39ce8.js";export{t as component};
---
+++ src/immutable/chunks/stores-0f01d1c1.js
@@ -0,0 +1,1 @@
+import"./index-b0368c0d.js";import{s as e}from"./singletons-d32ed029.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
