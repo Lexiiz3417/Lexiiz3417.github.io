d685b94a8d96ed4e1647678cc04494ed80eac34e
Lexiiz <48111598+Lexiiz3417@users.noreply.github.com>
2024-02-29 22:53:51 +0700

    up

---
+++ LCHP/lchp.js
@@ -0,0 +1,5 @@
+//tema
+document.getElementById('changeTheme').addEventListener('click', function() {
+    document.body.classList.toggle('dark-theme');
+    document.body.classList.toggle('light-theme');
+});
